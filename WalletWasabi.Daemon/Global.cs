037c05aff075756fd8c6945a4b66f8af8a00ee77
Lucas Ontivero <lucasontivero@gmail.com>
2024-02-06 12:28:20 -0300

    Do not use Tor for RegTest

--- WalletWasabi.Daemon/Global.cs
+++ WalletWasabi.Daemon/Global.cs
@@ -301,7 +301,7 @@ public class Global
 
 	private async Task StartTorProcessManagerAsync(CancellationToken cancellationToken)
 	{
-		if (Config.UseTor && Network != Network.RegTest)
+		if (Config.UseTor)
 		{
 			using (BenchmarkLogger.Measure(operationName: "TorProcessManager.Start"))
 			{
