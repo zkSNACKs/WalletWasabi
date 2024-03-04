09b58aba72e013524e74ce489161701be45d2600
Lucas Ontivero <lucasontivero@gmail.com>
2024-03-04 09:31:51 -0300

    Simplify `FeeProviders`

--- WalletWasabi.Tests/UnitTests/Wallet/WalletBuilder.cs
+++ WalletWasabi.Tests/UnitTests/Wallet/WalletBuilder.cs
@@ -61,7 +61,7 @@ public class WalletBuilder : IAsyncDisposable
 
 		var serviceConfiguration = new ServiceConfiguration(new UriEndPoint(new Uri("http://www.nomatter.dontcare")), Money.Coins(WalletWasabi.Helpers.Constants.DefaultDustThreshold));
 
-		HybridFeeProvider feeProvider = new(Synchronizer, null);
+		HybridFeeProvider feeProvider = new(new EventBus());
 		SmartBlockProvider blockProvider = new(BitcoinStore.BlockRepository, rpcBlockProvider: null, null, null, Cache);
 
 		WalletFactory walletFactory = new(DataDir, Network.RegTest, BitcoinStore, Synchronizer, serviceConfiguration, feeProvider, blockProvider);
